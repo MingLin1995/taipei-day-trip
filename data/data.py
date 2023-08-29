b8c44e3e49bdd22aede6395032156879b3265b14
Ming <ben014335@gamil.com>
2023-08-29 21:12:48 +0800

    新增景點資料庫以及取得資料的程式

---
+++ data/data.py
@@ -0,0 +1,82 @@
+import json
+import mysql.connector.pooling
+
+with open("taipei-attractions.json", "r", encoding="utf-8") as json_file:
+    data = json.load(json_file)
+results = data['result']['results']
+
+""" 先確認所需資訊 """
+""" print(results[0]["name"])  # 景點名稱
+print(results[0]["CAT"])  # 景點類別
+print(results[0]["description"])  # 景點介紹
+print(results[0]["address"])  # 景點位置（要排除"空白"符號，顯示完整地址）
+print(results[0]["direction"])  # 前往方式
+print(results[0]["MRT"])  # 鄰近捷運站 （陽明山有none）
+print(results[0]["latitude"])  # 緯度
+print(results[0]["longitude"])  # 經度
+print(results[0]["file"])  # 圖片網址（要整裡全部的網址） """
+
+""" 整理資料 """
+""" print(results[1]["address"].replace(" ", ""))
+
+file = results[0]["file"]
+# 全部轉換成小寫，用https://去分割
+image_links = file.lower().split('https://')[1:]  # 以 https:// 分隔，去掉第一個空元素
+# 補上https://，並篩選.jpg、.png結尾的資料
+image_urls = ['https://' +
+              link for link in image_links if link.endswith(('.jpg', '.png'))] """
+
+""" 存取資料 """
+attractions_data = []
+
+for result in results:
+    file = result["file"]
+    image_links = file.lower().split('https://')[1:]
+    image_urls = ['https://' +
+                  link for link in image_links if link.endswith(('.jpg', '.png'))]
+
+    attraction = {
+        "name": result["name"],
+        "CAT": result["CAT"],
+        "description": result["description"],
+        "address": result["address"].replace(" ", ""),
+        "direction": result["direction"],
+        "MRT": result["MRT"],
+        "latitude": result["latitude"],
+        "longitude": result["longitude"],
+        "file": image_urls
+    }
+    attractions_data.append(attraction)
+
+# print(attractions_data[1]["file"][7])  # 確定資料正確
+
+db = {
+    "host": "localhost",
+    "user": "root",
+    "password": "123456",
+    "database": "TP_data",
+    "pool_size": 8
+}
+
+connection_pool = mysql.connector.pooling.MySQLConnectionPool(**db)
+
+connection = connection_pool.get_connection()
+cur = connection.cursor()
+
+for attraction in attractions_data:
+    insert_query = "INSERT INTO attractions (name, category, description, address, transport, mrt, lat, lng) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
+    values = (attraction["name"], attraction["CAT"], attraction["description"], attraction["address"],
+              attraction["direction"], attraction["MRT"], attraction["latitude"], attraction["longitude"])
+    cur.execute(insert_query, values)
+
+    # https://cloud.tencent.com/developer/article/1406560
+    # 取得最後插入資料的主鍵值
+    attraction_id = cur.lastrowid
+    for image_url in attraction["file"]:
+        insert_image_query = "INSERT INTO images (attraction_id, image_url) VALUES (%s, %s)"
+        image_values = (attraction_id, image_url)
+        cur.execute(insert_image_query, image_values)
+
+connection.commit()
+cur.close()
+connection.close()
---
+++ data/data.sql
@@ -0,0 +1,108 @@
+SHOW DATABASES; 
+CREATE DATABASE TP_data; 
+
+USE TP_data; 
+SHOW TABLES;
+
+#刪除刪除刪除不要誤點
+DROP DATABASE TP_data; 
+
+CREATE TABLE attractions (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255),
+    category VARCHAR(255),
+    description TEXT,
+    address VARCHAR(255),
+    transport TEXT,
+    mrt VARCHAR(255),
+    lat DECIMAL(9, 6), /*顯示到小數點六位數，總共最多九位數*/
+    lng DECIMAL(9, 6)
+);
+SELECT * FROM attractions;
+
+CREATE TABLE images (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    attraction_id INT,
+    image_url VARCHAR(255),
+    FOREIGN KEY (attraction_id) REFERENCES attractions(id)
+);
+SELECT * FROM images;
+
+/*頁數查詢*/
+SELECT
+    a.id,
+    a.name,
+    a.category,
+    a.description,
+    a.address,
+    a.transport,
+    a.mrt,
+    a.lat,
+    a.lng,
+    GROUP_CONCAT(i.image_url) AS images /*將所有網址合併成一個字串，用","分隔*/
+FROM
+    attractions a
+JOIN
+    images i ON a.id = i.attraction_id
+GROUP BY
+    a.id /*依照id去分組，再將圖片網址合併*/
+LIMIT 24, 12; /*從查詢結果的第25筆記錄開始，顯示12筆*/
+
+/*關鍵字查詢*/
+SELECT
+	a.id,
+	a.name,
+	a.category,
+	a.description,
+	a.address,
+	a.transport,
+	a.mrt,
+	a.lat,
+	a.lng,
+	GROUP_CONCAT(i.image_url) AS images
+FROM
+	attractions a
+JOIN
+	images i ON a.id = i.attraction_id
+WHERE
+	a.name LIKE '%北%' OR a.mrt = '北' /*模糊景點名稱或完整捷運名稱*/
+GROUP BY 
+	a.id 
+LIMIT 12,12; /*總共16筆，12,0的時候，會有下一頁(還有4筆)*/
+
+/*景點編號*/
+SELECT
+    a.id,
+    a.name,
+    a.category,
+    a.description,
+    a.address,
+    a.transport,
+    a.mrt,
+    a.lat,
+    a.lng,
+    GROUP_CONCAT(i.image_url) AS images
+FROM
+    attractions a
+JOIN
+    images i ON a.id = i.attraction_id
+WHERE
+    a.id = '45';
+
+
+/*捷運查詢*/
+SELECT a.mrt, COUNT(*) AS num_attractions 
+FROM attractions a
+GROUP BY a.mrt
+ORDER BY num_attractions DESC, a.mrt; /*數量大到小，捷運名稱小到大*/
+
+SELECT a.mrt 
+FROM attractions a
+GROUP BY a.mrt
+ORDER BY COUNT(*) DESC, a.mrt; 
+
+
+
+
+
+
